4c11aea8247cd44e7c4bcc49c8a414e2b69fcd78
Brandon Whaley <redkrieg@gmail.com>
2013-04-07 15:17:54 -0400

    functional sparkline generation

--- spark.py
+++ spark.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
-import sys, json
+import sys
 
 spark_chars = u"▁▂▃▄▅▆▇█"
 
-print spark_chars
+def convert_to_float(i):
+    try:
+        return float(i)
+    except:
+        return None
+
+# Read all data from stdin, split by whitespace
+series_data_raw = [ i.strip() for i in sys.stdin.read().split() ]
+
+# Convert valid floats to float, discard remaining elements
+series_data = filter(lambda x: x is not None, map(convert_to_float, series_data_raw))
+
+maximum = max(series_data)
+
+if maximum == 0.0:
+    print "Cannot normalize when maximum is zero."
+    sys.exit(1)
+
+sparked_series_data = map(lambda x: spark_chars[int(x * 7 / maximum)], series_data)
+
+print ''.join(sparked_series_data)
